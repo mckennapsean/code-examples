3f8801c7476b154869b47ab54fa5d3b18b80fba1
Sean McKenna <mckennapsean@gmail.com>
2013-02-09 23:54:28 -0700

    continue adding license to header of source files

--- Java/Language/DNA.java
+++ Java/Language/DNA.java
@@ -1,4 +1,18 @@
-// by Sean McKenna on February 7th, 2010
+// Copyright 2013 Sean McKenna
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
 // takes input of DNA and converts to complementary strand (or RNA output)
 // A pairs with T (or U for RNA), & C with G
 // handles errors from input in the GUI
