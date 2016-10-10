d26329e274e25342da38d82060efbb30cc71a363
Sean McKenna <mckennapsean@gmail.com>
2016-10-09 22:31:21 -0600

    change license over to mit for #5

--- Java/System/inputOutput.java
+++ Java/System/inputOutput.java
@@ -1,18 +1,3 @@
-// Copyright 2013 Sean McKenna
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//        http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-//
-
 // takes basic input (integers, line-by-line) to output sums
 // input file ends with a 0, the sum is produced for each integer
 // that is, for a given integer N, compute the sum of integers between 1 and N, inclusive
